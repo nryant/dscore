4580ffd7b29fd7159f9c247cfd82597282994f2b
Neville Ryant <nryant@gmail.com>
2019-02-18 21:12:56 -0500

    Update UEM to allow for arbitrary number of scoring regions per file.

--- scorelib/uem.py
+++ scorelib/uem.py
@@ -7,45 +7,87 @@ file that will be scored.
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-from collections import defaultdict, Iterable, Mapping
+from collections import defaultdict
+try:
+    from collections import MutableMapping
+except ImportError:
+    from collections.abc import MutableMapping
 import itertools
 import os
 
-from .six import iteritems, iterkeys
+from intervaltree import IntervalTree
+
+from .six import iterkeys
 from .utils import format_float
 
 __all__ = ['gen_uem', 'load_uem', 'write_uem', 'UEM']
 
 
-class UEM(dict):
+class UEM(MutableMapping):
     """Un-partitioned evaluaion map (UEM).
 
     A UEM defines a mapping from file ids to scoring regions.
     """
-    def __setitem__(self, k, v):
-        if not isinstance(v, Iterable):
-            raise ValueError('Not a valid interval.')
-        v = list(v)
-        if len(v) != 2:
-            raise ValueError('Not a valid interval.')
-        onset, offset = v
-        onset = float(onset)
-        offset = float(offset)
-        if onset >= offset:
-            raise ValueError('Not a valid interval.')
-        super(UEM, self).__setitem__(k, (onset, offset))
+    def __init__(self, *args, **kwargs):
+        super(UEM, self).__init__()
+        self.update(*args, **kwargs)
 
-    def update(self, iterable, **kwargs):
-        if iterable is not None:
-            if isinstance(iterable, Mapping):
-                for k, v in iteritems(iterable):
-                    self.__setitem__(k, v)
-            else:
-                for k, v in iterable:
-                    self.__setitem__(k, v)
-        if kwargs:
-            for k, v in iteritems(kwargs):
-                self.__setitem__(k, v)
+    def __setitem__(self, fid, score_regions):
+        # Validate types. Expects sequence of (onset, offset) pairs.
+        invalid_type_msg = (
+            'Expected sequence of pairs. Received: %r (%s).' %
+            (score_regions, type(score_regions)))
+        try:
+            score_regions = [tuple(region) for region in score_regions]
+        except TypeError:
+            raise TypeError(invalid_type_msg)
+        for score_region in score_regions:
+            if len(score_region) != 2:
+                raise TypeError(invalid_type_msg)
+
+        # Validate that the (onset, offset) pairs are valid: no negative
+        # timestamps or negative durations.
+        def _convert_to_float(score_region):
+            onset, offset = score_region
+            try:
+                onset = float(onset)
+                offset = float(offset)
+            except ValueError:
+                raise ValueError(
+                    'Could not convert interval onset/offset to float: %s' %
+                    repr(score_region))
+            if onset >= offset or onset < 0:
+                raise ValueError(
+                    'Invalid interval (%.3f, %.3f) for file "%s".' %
+                    (onset, offset, fid))
+            return onset, offset
+        score_regions = [_convert_to_float(region) for region in score_regions]
+
+        # Merge overlaps. Use of intervaltree Incurs some additional overhead,
+        # but pretty compared to actual scoring.
+        tree = IntervalTree.from_tuples(score_regions)
+        tree.merge_overlaps()
+        score_regions = [(intrvl.begin, intrvl.end) for intrvl in tree]
+
+        self.__dict__[fid] = score_regions
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return '{}, UEM({})'.format(super(UEM, self).__repr__(), self.__dict__)
 
 
 def load_uem(uemf):
@@ -74,7 +116,7 @@ def load_uem(uemf):
         Evaluation map.
     """
     with open(uemf, 'rb') as f:
-        uem = UEM()
+        fid_to_score_regions = defaultdict(list)
         for line in f:
             if line.startswith(b';'):
                 continue
@@ -82,8 +124,8 @@ def load_uem(uemf):
             file_id = os.path.splitext(fields[0])[0]
             onset = float(fields[2])
             offset = float(fields[3])
-            uem[file_id] = (onset, offset)
-    return uem
+            fid_to_score_regions[file_id].append((onset, offset))
+    return UEM(fid_to_score_regions.items())
 
 
 def write_uem(uemf, uem, n_digits=3):
@@ -103,14 +145,14 @@ def write_uem(uemf, uem, n_digits=3):
     """
     with open(uemf, 'wb') as f:
         for file_id in sorted(iterkeys(uem)):
-            onset, offset = uem[file_id]
-            line = ' '.join([file_id,
-                             '1',
-                             format_float(onset, n_digits),
-                             format_float(offset, n_digits)
-                            ])
-            f.write(line.encode('utf-8'))
-            f.write(b'\n')
+            for onset, offset in sorted(uem[file_id]):
+                line = ' '.join([file_id,
+                                 '1',
+                                 format_float(onset, n_digits),
+                                 format_float(offset, n_digits)
+                                ])
+                f.write(line.encode('utf-8'))
+                f.write(b'\n')
 
 
 def gen_uem(ref_turns, sys_turns):
@@ -143,5 +185,5 @@ def gen_uem(ref_turns, sys_turns):
         offsets[turn.file_id].add(turn.offset)
     uem = UEM()
     for file_id in file_ids:
-        uem[file_id] = (min(onsets[file_id]), max(offsets[file_id]))
+        uem[file_id] = [(min(onsets[file_id]), max(offsets[file_id]))]
     return uem
